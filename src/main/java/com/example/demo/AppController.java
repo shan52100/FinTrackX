72b50ea249ae7deb04e0602238b98869b7e6785b
shan52100 <shanworkspace19@gmail.com>
2024-12-01 02:57:04 +0530

    finrepo

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+	@Autowired
+			private ProductService service;
+	@RequestMapping("/")
+	public String viewHomePage(Model model) {
+		List<Product> listProducts = service.ListAll();
+		model.addAttribute("listProducts",listProducts);
+		
+		return "index";
+	}
+	 
+		
+	
+}
